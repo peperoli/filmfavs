a9f3ca5f83a2013fcd8df057f062652a5f7c115a
peperoli <shagix.ix@gmail.com>
2023-04-04 16:31:10 +0200

    Enhancement: Add production base url

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
-"use client"
-
-import { useToken } from "../hooks/useToken"
-import { useState, useEffect } from "react"
-import { useRouter } from "next/navigation"
+import { Landing } from '@/components/Landing'
 
 export default function Home() {
-  const [clicked, setClicked] = useState(false)
-  const { data: token, status, fetchStatus } = useToken(clicked)
-  const router = useRouter()
-
-  useEffect(() => {
-    if (status === 'success') {
-      router.push(
-        `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/most-seen`
-      )
-      setClicked(false)
-    }
-    if (status === 'error') {
-      setClicked(false)
-    }
-  }, [status])
-  return (
-    <main>
-      <div className="container grid place-content-center h-screen mx-auto">
-        <h1 className="mb-8 text-7xl font-black">Your ratings and favourites</h1>
-        <div>
-          <button
-            onClick={() => setClicked(true)}
-            className="bg-white text-black/90 px-4 py-2 text-lg disabled:opacity-50"
-            disabled={fetchStatus === 'fetching'}
-          >
-            Let&apos;s Go!
-          </button>
-        </div>
-      </div>
-    </main>
-  )
+  return <Landing />
 }
---
+++ components/Landing.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useToken } from '../hooks/useToken'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export const Landing = () => {
+  const [clicked, setClicked] = useState(false)
+  const router = useRouter()
+  const { data: token, status, fetchStatus } = useToken(clicked)
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+  
+  useEffect(() => {
+    if (status === 'success') {
+      router.push(
+        `https://www.themoviedb.org/authenticate/${token}?redirect_to=${baseUrl}/most-seen`
+      )
+      setClicked(false)
+    }
+    if (status === 'error') {
+      setClicked(false)
+    }
+  }, [status])
+
+  return (
+    <main>
+      <div className="container grid place-content-center h-screen mx-auto">
+        <h1 className="mb-8 text-7xl font-black">Your ratings and favourites</h1>
+        <div>
+          <button
+            onClick={() => setClicked(true)}
+            className="bg-white text-black/90 px-4 py-2 text-lg disabled:opacity-50"
+            disabled={fetchStatus === 'fetching'}
+          >
+            Let&apos;s Go!
+          </button>
+        </div>
+      </div>
+    </main>
+  )
+}
